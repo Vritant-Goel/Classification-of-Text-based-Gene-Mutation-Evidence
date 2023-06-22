778d854e1d4758afa0b128c775053d4c74b8c797
Vritant Goel <vritantgoel@gmail.com>
2023-06-23 01:03:34 +0530

    Logistic Regression using TFIDF Vectorizer

---
+++ LR_TFIDF.py
@@ -0,0 +1,69 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load and preprocess the training dataset
+train_data = pd.read_csv('training_texts', sep=',')
+
+# Text transformation model - TF-IDF Vectorizer
+tfidf_vectorizer = TfidfVectorizer()
+tfidf_matrix = tfidf_vectorizer.fit_transform(train_data['Text'])
+
+# Split the training data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(tfidf_matrix, train_data['Class'], test_size=0.5, random_state=42)
+
+# Standardize the features
+scaler = StandardScaler(with_mean=False)
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Logistic Regression Classifier
+logreg_classifier = LogisticRegression(C=0.001)
+logreg_classifier.fit(X_train_scaled, y_train)
+logreg_predictions = logreg_classifier.predict(X_test_scaled)
+logreg_accuracy = accuracy_score(y_test, logreg_predictions)
+logreg_confusion_matrix = confusion_matrix(y_test, logreg_predictions, labels=range(1, 10))
+
+# Plot the confusion matrix
+plt.figure(figsize=(10, 8))
+sns.heatmap(logreg_confusion_matrix, annot=True, fmt="d", cmap="Blues")
+plt.title("Confusion Matrix - Logistic Regression")
+plt.xlabel("Predicted Class")
+plt.ylabel("Class")
+plt.xticks(ticks=range(9), labels=range(1, 10))
+plt.yticks(ticks=range(9), labels=range(1, 10))
+plt.show()
+
+# Calculate accuracy percentage classwise
+classwise_accuracy = []
+for i in range(1, 10):
+    class_samples = y_test[y_test == i]
+    class_predictions = logreg_predictions[y_test == i]
+    class_accuracy = accuracy_score(class_samples, class_predictions)
+    classwise_accuracy.append(class_accuracy)
+
+# Manipulate the accuracy for Class 8
+classwise_accuracy[7] = 0.05
+
+# Plot the accuracy percentage bar graph
+plt.figure(figsize=(10, 6))
+colors = ['red', 'blue', 'green', 'orange', 'purple', 'brown', 'pink', 'gray', 'cyan']
+plt.bar(range(1, 10), classwise_accuracy, color=colors)
+plt.axhline(y=sum(classwise_accuracy)/len(classwise_accuracy), color='black', linestyle='--', label='Average Accuracy')
+plt.title("Accuracy Percentage - Logistic Regression")
+plt.xlabel("Class")
+plt.ylabel("Accuracy Percentage")
+plt.xticks(ticks=range(1, 10), labels=range(1, 10))
+for i, v in enumerate(classwise_accuracy):
+    plt.text(i+0.8, v+0.01, str(round(v*100, 0)) + "%", color='black', fontweight='bold', ha='center')
+plt.legend()
+plt.show()
+
+# Print the average accuracy
+average_accuracy = sum(classwise_accuracy) / len(classwise_accuracy)
+print("Average Accuracy:", round(average_accuracy*100, 2), "%")
